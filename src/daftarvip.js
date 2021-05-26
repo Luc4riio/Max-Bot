d9f06621c5121e4ec8495f5b68bea086a84d739c
LucasssMODS <lucas.vinicius.oliveira.2007@gmail.com>
2021-05-26 17:51:48 -0300

    Lucasse Mods War

---
+++ src/daftarvip.js
@@ -0,0 +1,15 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*PREÇO DE LISTA VIP :*
+
+-R$2,50 > Acessar recursos ViP
+-R$5,00 > Recursos VIP + Insira o bot no seu grupo!
+
+*SE QUER REGISTAR VIP :*
+
+*Proprietário do bate-papo BOT :*
+
+_wa.me/5592984928452 ou digite *${prefix}owner*_ `
+}
+exports.daftarvip = daftarvip
---
+++ src/help.js
@@ -0,0 +1,105 @@
+const help = (pushname, prefix) => {
+  return`╭═⊷〘  MAX BOT 〙═⊷❍
+┃
+┣═〘 INFORMAÇÕES 〙═⊷❍
+┣𖥻ꦼꦽ➳❒ USUÁRIO: ${pushname}
+┣𖥻ꦼꦽ➳❒ PREFIXO: *${prefix}*
+┣𖥻ꦼꦽ➳❒ RAPIDEZ: *${prefix}ping*
+┣𖥻ꦼꦽ➳❒ DONO: *${prefix}owner*
+┣𖥻ꦼꦽ➳❒ RELATAR BUG: ${prefix}report
+╰════════════⊷❍
+
+╭〘 PRINCIPAIS 〙
+┃│
+┃│❍➣ *${prefix}daftarvip*
+┃│❍➣ *${prefix}tomp3*
+┃│❍➣ *${prefix}menu*
+┃│❍➣ *${prefix}help*
+┃│❍➣ *${prefix}info*
+┃│❍➣ *${prefix}sticker*
+┃│❍➣ *${prefix}fsticker*
+┃│❍➣ *${prefix}toimg*
+┃│❍➣ *${prefix}speed*
+┃│❍➣ *${prefix}wame*
+┃│❍➣ *${prefix}wa.me*
+┃│❍➣ *${prefix}blocklist*
+╰══════════⊷❍
+
+╭  〘 PROPRIETÁRIO 〙
+┃│
+┃│❍➣ *${prefix}setprefix*
+┃│❍➣ *${prefix}dado*
+┃│❍➣ *${prefix}leave*
+┃│❍➣ *${prefix}bc*
+┃│❍➣ *${prefix}addvip*
+╰══════════⊷❍
+
+╭〘  OUTROS 〙
+┃│
+┃│❍➣ *${prefix}qrcode* (somemte premium)
+┃│❍➣ *${prefix}meme*
+┃│❍➣ *${prefix}lucas*
+┃│❍➣ *${prefix}tahta*
+┃│❍➣ *${prefix}tp*
+┃│❍➣ *${prefix}ocr*
+┃│❍➣ *${prefix}db*
+┃│❍➣ *${prefix}modapk*
+┃│❍➣ *${prefix}chentai*
+┃│❍➣ *${prefix}wppim*
+┃│❍➣ *${prefix}xvideos*
+┃│❍➣ *${prefix}xnxx*
+┃│❍➣ *${prefix}play*
+┃│❍➣ *${prefix}yt2mp3*
+┃│❍➣ *${prefix}ytsearch*
+┃│❍➣ *${prefix}nulis*
+┃│❍➣ *${prefix}tulis*
+┃│❍➣ *${prefix}url2img*
+┃│❍➣ *${prefix}attp*
+┃│❍➣ *${prefix}gtts*
+╰══════════⊷❍
+
+╭〘  MENU DO GRUPO〙⊷❍
+┃│
+┃│❍➣ *${prefix}destrava*
+┃│❍➣ *${prefix}opengc*
+┃│❍➣ *${prefix}closegc*
+┃│❍➣ *${prefix}promote*
+┃│❍➣ *${prefix}demote*
+┃│❍➣ *${prefix}add*
+┃│❍➣ *${prefix}kick*
+┃│❍➣ *${prefix}listadmins*
+┃│❍➣ *${prefix}linkgroup*
+┃│❍➣ *${prefix}tagall*
+┃│❍➣ *${prefix}tagall3*
+┃│❍➣ *${prefix}hidemarcar*
+┃│❍➣ *${prefix}welcome 1/0*
+┃│❍➣ *${prefix}setdesc*
+┃│❍➣ *${prefix}wait*
+┃│❍➣ *${prefix}clearchat*
+╰════════════════⊷❍
+
+╭〘 INTELIGÊNCIA 〙
+┃│
+┃│❍➣ *${prefix}simih 1*
+┃│❍➣ *${prefix}simih 0*
+╰═════════════⊷❍
+
+╭〘 INTERAÇÕES 〙
+┃│
+┃│❍➣ *${prefix}gay*
+┃│❍➣ *${prefix}cheguei*
+┃│❍➣ *${prefix}botfdp*
+┃│❍➣ *${prefix}botgay*
+┃│❍➣ *${prefix}botbaiano*
+┃│❍➣ *${prefix}botputa*
+┃│❍➣ *${prefix}botfalido*
+╰═════════════⊷❍
+
+╭〘 Para falar com o criador  〙
+╠
+╠➥${prefix}dono
+╠➥${prefix}owner
+╰════════════⊷❍
+`
+}
+exports.help = help
